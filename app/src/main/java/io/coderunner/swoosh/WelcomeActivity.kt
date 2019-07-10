4cb98f60d21ef18883e62a8a6bcf3628fcdc9bb0
Tim Bennett <2475478+bennetimo@users.noreply.github.com>
2019-07-10 23:17:39 +0100

    lesson-03

--- app/src/main/java/io/coderunner/swoosh/WelcomeActivity.kt
+++ app/src/main/java/io/coderunner/swoosh/WelcomeActivity.kt
@@ -1,12 +1,18 @@
 package io.coderunner.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        btnGetStarted.setOnClickListener {
+            startActivity(Intent(this, LeagueActivity::class.java))
+        }
     }
 }
